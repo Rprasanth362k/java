5fba25aed2d454fada859f561e9f6fd1888a2072
prasanth362k <prasanth362k@gmail.com>
2024-06-18 15:17:43 +0530

    first commit

---
+++ Allinone.java
@@ -0,0 +1,64 @@
+class Allinone
+{
+static int total,full;
+public static void main(String args[])
+{
+
+System.out.println(" well come to java prasath");
+  Allinone method = new Allinone();
+total=method.callmethod(10000);
+  full=total-100;
+System.out.println("sale the product "+ full);
+}
+public  static int callmethod(int b)
+
+{
+int a = 1000;
+
+int c = a+b;
+System.out.println("Buy the product cycle "+c);
+return c;
+}
+
+}
+
+
+
+/* java 
+1) How to Create class - Done
+2) How to add Main method - Done # if name == "main":
+3) How to add local variables
+4) How to create Object - Done 
+5) How to call methods - Done 
+6) How to pass arguments - Done
+7) How to return values - Done 
+8) global variables 
+9) static variables
+10) non-static variables
+11) Constructors, Constructor Overloading
+12) Method Overloading
+13) Inheritance - Parent Class  - Child Class
+14) Types of Inheritance
+15) Method Overriding
+16) Abstract Class
+17) Interface
+18) Dynamic Binding
+19) Type Casting - Upcasting, Downcasting
+20) Covariant return datatype
+21) Encapsulation - 
+22) import package.classname
+
+
+
+Types of Variables: 
+1. Local Variables
+2. Fields
+3. Arguments
+
+Types of Arguments in Python: 
+1. Positional Arguments
+2. Default Arguments
+3. Keyword Arguments
+
+
+Types of Datatypes: */
